24b1ad3ca0bf4bd9c0ae58a17afc750676ac6e00
Thomas Braibant <thomas.braibant@gmail.com>
2014-07-26 20:40:27 +0200

    Byte

--- byte.ml
+++ byte.ml
@@ -1,13 +1,21 @@
 type t = int 			(* 0 to 255 *)
-let succ i = (i + 1) mod 256
-let pred i = (i - 1) mod 256
-let add  i j = (i + j) mod 256
-let sub i j = (i - j) mod 256
-let mul i j = (i * j) mod 256
-let div i j = (i / j) mod 256
-let _land i j = i land j
+
+let norm x =
+  let r = a mod 256 in
+  if r < 0
+  then r + 256
+  else r
+
+let succ i   = norm (i + 1)
+let pred i   = norm (i - 1)
+let add  i j = norm (i + j)
+let sub i j  = norm (i - j)
+let mul i j  = norm (i * j)
+let div i j  = norm (i / j)
+
+let _land i j= i land j
 let _lor i j = i lor j
-let _lxor i j = i lxor j
+let _lxor i j= i lxor j
 
 let lt i j = i < j
 let eq i j = i = j
