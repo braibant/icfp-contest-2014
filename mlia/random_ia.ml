3b7f3d2897844659325d60d3d04ad871d6d25f33
Gabriel Scherer <gabriel.scherer@gmail.com>
2014-07-27 10:55:44 +0200

    random_ia.ml: avoid U-turns

--- mlia/random_ia.ml
+++ mlia/random_ia.ml
@@ -1,10 +1,22 @@
 let modulo a b =
   a - (a / b) * b
 
+let state = (0, 0)
+
+let opposite a b =
+  if (a - b) = 2 then true
+  else if (a - b) = -2 then true
+  else false
+
 let random n = modulo (n * 1103515245 + 12345) 2147483648
-let step state _world =
-  let next_state = random state in
-  let direction = modulo (state / 54321) 4 in
-  (next_state, direction)
+let step (rnd, old_dir) _world =
+  let rnd = random rnd in
+  let new_dir = modulo (rnd / 54321) 4 in
+  let direction =
+    (* do not U-turn *)
+    if opposite old_dir new_dir
+    then old_dir
+    else new_dir in
+  ((rnd, direction), direction)
   
-let main_gcc = (0, step)
+let main_gcc = (state, step)
