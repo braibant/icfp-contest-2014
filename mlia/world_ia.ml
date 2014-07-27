ceb91474e169591240e75d95dc0fdc5d55efb2f0
Gabriel Scherer <gabriel.scherer@gmail.com>
2014-07-27 12:29:27 +0200

    world_ia: stdlib improvements

--- mlia/world_ia.ml
+++ mlia/world_ia.ml
@@ -79,11 +79,32 @@ let rec nth n = function
     else nth (n-1) xs
 
 let rec find p = function
-  | [] -> assert false
+  | [] -> None
   | x::xs ->
-    if p x then x
+    if p x then Some x
     else find p xs
 
+let rec mem eq x = function
+  | [] -> false
+  | x :: xs ->
+    eq x || mem eq x xs
+
+type 'a option =
+| None
+| Some of 'a
+
+type iter_status =
+| Continue
+| Stop
+
+let rec fold_left_stop f acc = function
+  | [] -> Continue, acc
+  | x::xs ->
+    match f acc x with
+      | Stop, v -> Stop, v
+      | Continue, acc -> fold_left_stop f acc xs
+
+(** Domain-specific library *)
 let get map (i, j) =
   nth i (nth j map)
 
@@ -96,13 +117,18 @@ let next_pos direction (x, y) = match direction with
   | Down  -> (x, y+1)
   | Left  -> (x-1, y)
 
-let directions = [Up; Right; Down; Left]
+(* let directions = [Up; Right; Down; Left] *)
+let directions = [Left; Down; Right; Up]
 
 let step state world =
   let (map, lambda, _ghost, _fruit) = world in
   let (_vita, pos, _dir, _lives, _score) = lambda in
   let dir =
-    find (fun dir -> free map (next_pos dir pos)) directions
+    match
+      find (fun dir -> free map (next_pos dir pos)) directions
+    with
+      | None -> assert false
+      | Some dir -> dir
   in
   (state, dir)
 
