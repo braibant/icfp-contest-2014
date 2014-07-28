ce010c532fec8e03a14d7c11ee63e1cd75469440
Gabriel Scherer <gabriel.scherer@gmail.com>
2014-07-28 12:40:38 +0200

    improved map in test.ml

--- mlia/test.ml
+++ mlia/test.ml
@@ -1,19 +1,35 @@
 #mod_use "world.ml";;
 open World
 
-#mod_use "world_ia.ml";;
-open World_ia;;
+#mod_use "world_ffi.ml";;
+open World_ffi;;
 
-let map =
+let of_char = function
+  | '-' -> Empty
+  | 'X' -> Wall
+  | 'o' -> Pill
+  | 'O' -> Power_pill
+  | _ -> invalid_arg "of_char"
+
+let list_of_string str =
+  let li = ref [] in
+  for i = String.length str - 1 downto 0 do
+    li := str.[i] :: !li;
+  done;
+  !li
+
+let map = List.map (fun str -> List.map of_char (list_of_string str))
   [
-    [Wall ; Wall ; Wall ; Wall ; Wall ;];
-    [Wall ; Wall ; Empty; Wall ; Wall ;];
-    [Wall ; Empty; Empty; Empty; Wall ;];
-    [Wall ; Wall ; Empty ; Wall ; Wall ;];
-    [Wall ; Wall ; Empty ; Wall ; Wall ;];
-    [Wall ; Wall ; Wall ; Wall ; Wall ;];
+       (*0123456789012345 *)
+(* 0 *) "XXXXXXXXXXXXXXXX";
+(* 1 *) "XXXXXXXX-XXXXXXX";
+(* 2 *) "XXXXXXXX-XXXXXXX";
+(* 3 *) "XXXXXXXX---XXXXX";
+(* 4 *) "XXXXXXXX-XXXXXXX";
+(* 5 *) "XXXXXXXX-XXXXXXX";
+(* 6 *) "XXXXXX-----XXXXX";
+(* 7 *) "XXXXXXX-o-XXXXXX";
+(* 8 *) "XXXXXXXXXXXXXXXX";
   ]
-(*
-# bfs map [] (2,2);;
-- : direction option = Some Down
-*)
+
+let map_start_pos = (8,1)
