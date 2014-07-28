7a2eae14f3dd7700e325c84407475d8d0795de01
Gabriel Scherer <gabriel.scherer@gmail.com>
2014-07-28 10:52:57 +0200

    cleanup slow_bfs_ia.ml

--- mlia/slow_bfs_ia.ml
+++ mlia/slow_bfs_ia.ml
@@ -1,137 +1,14 @@
-(** Avoid polymorphic equality *)
-let eq_int m n = (m : int) = n
+open! World_ffi
 
-let rec eq_list eq la lb = match la, lb with
-  | [], [] -> true
-  | x::xs, y::ys -> eq x y && eq_list eq xs ys
-  (* note: or-patterns are not well-supported *)
-  | [], _::_ -> false
-  | _::_, [] -> false
-
-let eq_pair eqa eqb (a,b) (a',b') =
-  eqa a a' && eqb b b'
-
-(** World FFI *)
-type ghost_vitality =
-| (* 0 *) Standard
-| (* 1 *) Fright_mode
-| (* 2 *) Invisible
-
-type square =
-| (* 0 *) Wall
-| (* 1 *) Empty
-| (* 2 *) Pill
-| (* 3 *) Power_pill
-| (* 4 *) Fruit
-| (* 5 *) Lambda_start
-| (* 6 *) Ghost_start
-
-type direction =
-| (* 0 *) Up
-| (* 1 *) Right
-| (* 2 *) Down
-| (* 3 *) Left
-
-type location = int * int
-
-type fruit_status = int
-
-type lambda_status =
-  ( lambda_vitality
-  * location
-  * direction
-  * lives
-  * score )
-and lambda_vitality = int
-and lives = int
-and score = int
-
-(*
-type lambda_status = {
-  vitality : int;
-  loc : location;
-  dir : direction;
-  lives : int;
-  score : int;
-}
-*)
-
-(*
-type ghost_status = {
-  vitality : ghost_vitality;
-  loc : location;
-  dir : direction;
-}
-*)
-
-type ghost_status =
-  ( ghost_vitality
-  * location
-  * direction )
-(*
-type t = {
-  map : map;
-  lambda : lambda_status;
-  ghosts : ghost_status list;
-  fruits : fruit_status;
-}
-*)
-type world =
-  ( map
-  * lambda_status
-  * ghost_status list
-  * fruit_status )
-and map = square list list
-
-
-(** Standard Library *)
-let rec nth n = function
-  | [] -> assert false
-  | x::xs ->
-    if eq_int n 0 then x
-    else nth (n-1) xs
-
-let rec find p = function
-  | [] -> None
-  | x::xs ->
-    if p x then Some x
-    else find p xs
-
-let rec mem eq x = function
-  | [] -> false
-  | x' :: xs ->
-    eq x x' || mem eq x xs
-
-type 'a option =
-| None
-| Some of 'a
-
-type iter_status =
-| Continue
-| Stop
-
-let rec fold_left f acc = function
-  | [] -> acc
-  | x::xs -> fold_left f (f acc x) xs
-
-let rec fold_left_stop f acc = function
-  | [] -> Continue, acc
-  | x::xs ->
-    match f acc x with
-      | Stop, v -> Stop, v
-      | Continue, acc -> fold_left_stop f acc xs
-
-let rec list_map f = function
-  | [] -> []
-  | x::xs ->
-    let a = f x in a :: list_map f xs
+open! Lib
+open! Lib_list
 
 (** Domain-specific library *)
 let eq_pos : location -> location -> bool =
   fun (x, y) (x', y') -> eq_int x x' && eq_int y y'
 
 let get map (i, j) =
-  nth i (nth j map)
+  list_nth' i (list_nth' j map)
 
 let free map pos =
   get map pos <> Wall
@@ -150,7 +27,7 @@ let good_square square =
   || square = Power_pill
   || square = Fruit
 
-let mem_pos pos li = mem eq_pos pos li
+let mem_pos pos li = list_mem eq_pos pos li
 
 let bfs map ghosts pos =
   let rec loop old cur_gen next_gen =
@@ -165,7 +42,7 @@ let bfs map ghosts pos =
       else if good_square (get map pos) then Some start_dir
       else
         let next_gen =
-          fold_left
+          list_fold_left
             (fun next_gen dir ->
               let pos = next_pos dir pos in
               if not (free map pos) || mem_pos pos ghosts then next_gen
@@ -175,7 +52,7 @@ let bfs map ghosts pos =
         loop (pos :: old) cur_gen next_gen
   in
   let first_gen =
-    fold_left
+    list_fold_left
       (fun gen dir ->
         let pos = next_pos dir pos in
         if not (free map pos) then gen
