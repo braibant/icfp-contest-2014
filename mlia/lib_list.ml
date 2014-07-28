698a71c47273490ed61d6d4a1e491f2694b0bc99
Gabriel Scherer <gabriel.scherer@gmail.com>
2014-07-28 10:44:37 +0200

    rename lib_list

--- mlia/lib_list.ml
+++ mlia/lib_list.ml
@@ -1,45 +1,44 @@
-let rec nth n = function
+let rec list_nth n = function
   | [] -> None
   | x::xs ->
     if n = 0 then Some x
-    else nth (n-1) xs
+    else list_nth (n-1) xs
 
-let rec nth' n = function
+let rec list_nth' n = function
   | [] -> assert false
   | x::xs ->
     if n = 0 then x
-    else nth' (n-1) xs
+    else list_nth' (n-1) xs
 
-let rec find p = function
+let rec list_find p = function
   | [] -> None
   | x::xs ->
     if p x then Some x
-    else find p xs
+    else list_find p xs
 
-let rec mem
+let rec list_mem
     (eq: 'a -> 'a -> bool)
     (x:'a) : 'a list -> bool
   = function
   | [] -> false
   | y :: xs ->
-    eq x y || mem eq x xs
+    eq x y || list_mem eq x xs
 
 
 type iter_status =
 | Continue
 | Stop
 
-let rec fold_left f acc = function
+let rec list_fold_left f acc = function
   | [] -> acc
-  | x::xs -> fold_left f (f acc x) xs
+  | x::xs -> list_fold_left f (f acc x) xs
 
-let rec fold_left_stop f acc = function
+let rec list_fold_left_stop f acc = function
   | [] -> Continue, acc
   | x::xs ->
     match f acc x with
       | Stop, v -> Stop, v
-      | Continue, acc -> fold_left_stop f acc xs
-
+      | Continue, acc -> list_fold_left_stop f acc xs
 
 let rec list_map f = function
   | [] -> []
@@ -52,9 +51,9 @@ let list_length li =
   | _::li -> loop (acc+1) li
   in loop 0 li
 
-let rec list_rev acc l =
+let rec list_rev_append l acc =
   match l with
     | [] -> acc
-    | t::q -> list_rev (t::acc) q
+    | t::q -> list_rev_append q (t::acc)
 
-let list_rev l = list_rev [] l
+let list_rev l = list_rev_append l []
