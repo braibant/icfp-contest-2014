9da5acaa72633b12fbab7b1e9deb211008617f48
Thomas Braibant <thomas.braibant@gmail.com>
2014-07-28 13:54:26 +0200

    With fruit

--- mlia/graph_local.ml
+++ mlia/graph_local.ml
@@ -179,7 +179,7 @@ let dfs (map: square vect vect) graph ghosts_places pos length fright=
           match content with
             | Pill -> value + 10, fright
             | Power_pill -> value + 50, fright + 20
-            | Fruit -> value + 50, fright
+            | Fruit -> value + 15, fright
             | _ -> value, fright
         in
         let value =
